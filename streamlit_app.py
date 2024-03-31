77c2ef5fa107469390fc14e5b38d128f3ca59bec
Sanam20302 <76154182+Sanam20302@users.noreply.github.com>
2024-03-31 20:00:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 st.title("Hey all")
-name = st.text_input("Type your name")
- st.write(name, "How are you?")
- bla = st.text_input("FIne or Not fine")
- if bla == "Fine":
-  st.write("Cool!")
+bla = st.text_input("How are you? fine/not fine")
+ if bla == "fine":
+  st.write("Cool!") 
  else:
-  st.write("Poor guy, deal with it...hehe")
+  st.write("Whatever deal with it...hehe")
