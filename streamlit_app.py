2332c68c6e1c18572d7e9c9a2a6b791143d73803
Sanam20302 <76154182+Sanam20302@users.noreply.github.com>
2024-03-31 20:04:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ st.title("Hey all")
 bla = st.text_input("How are you? fine/not fine")
 if bla == "fine":
   st.write("Cool!") 
-elif bla == " ":
+elif bla == "":
   print("type something")
 else:
   st.write("Whatever deal with it...hehe")
